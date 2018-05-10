f3bb2343d0e8e82165e8b58e6d06dfb1bafad004
Ahmadali <ahmadali.azizov@outlook.com>
2018-05-10 14:01:22 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,6 +2,7 @@ def roll_call_dwarves(dwarves)
   dwarves.each.with_index(1) do |name, index|
     puts "#{index}. #{name}"
 end
+end
 
 def summon_captain_planet(planeteer_calls)
  planeteer_calls.collect {|veggies| veggies.upcase + "!"}
@@ -11,10 +12,12 @@ def long_planeteer_calls(calls_long)
   calls_long.any? do |word|
     word.length > 4
   end
+end
 
 def find_the_cheese(cheddar_cheese)
    cheese_types = ["cheddar", "gouda", "camembert"]
 
   cheddar_cheese = cheddar_cheese.find do |cheese|
   cheese_types.include?(cheese)
+end
 end
